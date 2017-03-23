6c14fcd609708d08b9ff22ef6f99196c41353702
RaiManish3 <manish.07.1996@gmail.com>
2017-03-24 05:11:22 +0530

    returning the whole student info

--- Student_Search/st_search.py
+++ Student_Search/st_search.py
@@ -2,27 +2,33 @@ import urllib.request as ul
 import urllib.parse
 import re
 
-x =input()
-the_url = 'http://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchRes.jsp?sbm=Y'
-values= {'numtxt':x,'typ':'stud'}
+# x is the roll no of the student about whom you want to gather info
+def getStudentData(x):
+	the_url = 'http://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchRes.jsp?sbm=Y'
+	values= {'numtxt':x,'typ':'stud'}
 
-data = urllib.parse.urlencode(values)
-data = data.encode('utf-8')
+	data = urllib.parse.urlencode(values)
+	data = data.encode('utf-8')
 
-req = ul.Request(the_url,data)
-resp = ul.urlopen(req)
+	req = ul.Request(the_url,data)
+	resp = ul.urlopen(req)
 
-respData = resp.read()
-#readData has source code of result
+	respData = resp.read()
+	#readData has source code of result
 
-fields = re.findall(r'<b>(.*?)</b>',str(respData))
-result = re.findall(r'\\t([A-Za-z0-9]+[\w\.\d\s&\+\-,@]*)\\r',str(respData))
-result.insert(5,re.findall(r'mailto:(.*?)\"',str(respData))[0])
-del fields[0]
-del fields[7]
+	fields = re.findall(r'<b>(.*?)</b>',str(respData))
+	result = re.findall(r'\\t([A-Za-z0-9]+[\w\.\d\s&\+\-,@]*)\\r',str(respData))
+        # for mail
+	result.insert(5,re.findall(r'mailto:(.*?)\"',str(respData))[0])
+	del fields[0]
+	del fields[7]
+        # for img
+	img = re.findall(r'img src=\"http:(.*?)\"',str(respData))
+	img[0]='http:'+img[0]
 
-student_dic={}
-for x in range(len(result)):
-            student_dic[fields[x]]=result[x]
-
-print (student_dic)
+	student_dic={}
+	for x in range(len(result)):
+		student_dic[fields[x]]=result[x]
+	student_dic['image']=img[0]
+	#return student_dic[' E-Mail: ']
+	return (student_dic)
